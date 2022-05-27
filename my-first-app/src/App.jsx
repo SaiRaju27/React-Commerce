07096e17861e7177522d44c147361b3ce5f212db
SaiRaju27 <kraju9589@gmail.com>
2022-05-27 12:46:03 +0530

    Create App.jsx and added bootstrap packages

---
+++ my-first-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+export class NavBar extends Component {
+    render() {
+        return (
+            <>
+                <div class="highlight">
+                    <h1>Hello </h1>
+                    <p>hai</p>
+                </div>
+            </>
+        )
+
+    }
+}
--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import { NavBar } from './App.jsx';
+import "./index.css"
 
-var s = <h1>React App</h1>
-ReactDOM.render(s, document.getElementById("root"));
+// var s = <h1 class="container"><button class="btn btn-success">React App</button></h1>
+ReactDOM.render(<NavBar />, document.getElementById("root"));
+// console.log(s);
